27c4756b097c41dfc945cd206d05b0168b5cb36f
Aleksandra Janczewska <ola.janczewska@gmail.com>
2020-08-04 22:59:13 +0200

    change style and class names

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-const name = 'Ola';
+const name = "Ola";
 const age = 20;
 console.log(`Witaj w konsoli!
 Jeżeli dotarłeś aż tu, to znaczy, że jesteś zainteresowany jak działa moja strona,
 Spokojnie, nie mam nic do ukrycia:)`);
+
+
