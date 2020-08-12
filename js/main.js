c990f65fb1e8dbac7f5f20f414e2dd020267f256
Aleksandra Janczewska <ola.janczewska@gmail.com>
2020-08-12 20:37:44 +0200

    add media queries, hamburger menu

--- js/main.js
+++ js/main.js
@@ -4,16 +4,15 @@ console.log(`Witaj w konsoli!
 Jeżeli dotarłeś aż tu, to znaczy, że jesteś zainteresowany jak działa moja strona,
 Spokojnie, nie mam nic do ukrycia:)`);
 
-
-const addText = (classSelector, content) => {
-    document.querySelector(classSelector).innerHTML = content;
-
-}
-
-addText(".projects_section--js", "Przykładowy tekst uzupełninony przez JS:)")
-
 function greeting(name, age) {
-    console.log(`Witaj! Moje imie to ${name} i mam ${age} lat.`)
+  console.log(`Witaj! Moje imie to ${name} i mam ${age} lat.`);
 }
 
-greeting(name, age)
+greeting(name, age);
+
+const hamburgerMenu = document.querySelector(".hamburger--js");
+
+hamburgerMenu.addEventListener("click", () => {
+  const nav = document.querySelector('.navigation--js');
+  nav.classList.toggle('navigation--open');
+});
